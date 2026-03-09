3cdb8d6552affaa13237b50a748d9274014b3b8a
tecsoftapps <rubilyndocs@gmail.com>
2026-03-08 23:20:45 -0700

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,133 @@
+export const apps = [
+  {
+    name: 'Extract Frames from Video',
+    artwork: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/51/fd/14/51fd1400-baaf-d600-fbb3-87221783d965/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/540x540bb.webp',
+    url: 'https://apps.apple.com/us/app/extract-frames-from-video/id6759852671',
+    genre: 'Photo & Video'
+  },
+  {
+    name: 'Pinoy Lingo for iMessage',
+    artwork: 'https://is1-ssl.mzstatic.com/image/thumb/Purple128/v4/bb/88/a3/bb88a30e-05d6-f019-57dc-81706a853cf9/iMessage_App_Icon-1x_U007emarketing-0-0-GLES2_U002c0-512MB-sRGB-0-0-0-85-181-0-0-0-0.png/460x344sr.png',
+    url: 'https://apps.apple.com/us/app/pinoy-lingo-for-imessage/id1170816943?uo=4',
+    genre: 'Stickers'
+  },
+  {
+    name: 'Blessings Stickers',
+    artwork: 'https://is4-ssl.mzstatic.com/image/thumb/Purple71/v4/46/07/d6/4607d621-7960-0dcb-720b-7452239ad1d4/iMessage_App_Icon-1x_U007emarketing-0-0-GLES2_U002c0-512MB-sRGB-0-0-0-85-181-0-0-0-0.png/460x344sr.png',
+    url: 'https://apps.apple.com/us/app/blessings-stickers/id1156361305',
+    genre: 'Stickers'
+  },
+  {
+    name: 'Faith Stickers',
+    artwork: 'https://is4-ssl.mzstatic.com/image/thumb/Purple123/v4/52/af/a8/52afa8e3-704e-74fa-64ff-86aeeb8c7c04/iMessage_App_Icon-0-0-1x_U007emarketing-0-0-0-0-0-3-sRGB-0-0-0-GLES2_U002c0-512MB-85-181-0-0.png/460x344sr.png',
+    url: 'https://apps.apple.com/us/app/faith-stickers-for-imessage/id1462157801?ls=1',
+    genre: 'Stickers'
+  },
+  {
+    name: 'Drops - Your IV Drip Rate Companion',
+    artwork: 'https://is4-ssl.mzstatic.com/image/thumb/Purple7/v4/05/23/28/0523285a-6a40-7f37-3c7f-7de84f736f1e/source/512x512bb.jpg',
+    url: 'https://apps.apple.com/us/app/drops-your-iv-drip-rate-companion/id991594421?uo=4',
+    genre: 'Medical'
+  },
+  {
+    name: 'Chinese Blessings',
+    artwork: 'https://is5-ssl.mzstatic.com/image/thumb/Purple113/v4/2b/71/83/2b7183e1-a63e-ca45-9844-74d65eca8d7b/iMessage_App_Icon-0-0-1x_U007emarketing-0-0-0-0-0-3-sRGB-0-0-0-GLES2_U002c0-512MB-85-181-0-0.png/460x344sr.png',
+    url: 'https://apps.apple.com/us/app/chinese-blessings/id1495567535?ls=1',
+    genre: 'Stickers'
+  },
+  {
+    name: 'St.Patrick Lucky Stickers',
+    artwork: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/cb/83/f5/cb83f5c9-39bf-023f-5854-8fb79827464c/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/540x540bb.webp',
+    genre: 'Stickers'
+  },
+  {
+    name: 'Autumn Greetings',
+    artwork: 'https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/03/d5/93/03d5939e-9a07-32cf-5268-f52709b7bf61/AppIcon-0-0-1x_U007emarketing-0-0-0-0-0-5-sRGB-0-0-0-GLES2_U002c0-512MB-85-181-0-0.png/1024x768bb.png',
+    url: 'https://apps.apple.com/us/app/autumn-greetings/id1425749493',
+    genre: 'Stickers'
+  },
+  {
+    name: 'Christmas Greetings Animated',
+    artwork: 'https://is1-ssl.mzstatic.com/image/thumb/Purple112/v4/d7/3a/a0/d73aa0a9-8b1b-1b54-e2a5-ae4387393734/AppIcon-0-0-1x_U007emarketing-0-0-0-0-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-181-0-0.png/1024x768bb.png',
+    url: 'https://apps.apple.com/us/app/christmas-greetings-animated/id1182601223',
+    genre: 'Stickers'
+  },
+  {
+    name: 'Happy New Year 2023 Animated',
+    artwork: 'https://is1-ssl.mzstatic.com/image/thumb/Purple112/v4/01/38/24/013824c4-c3cd-83b1-57fd-6b72d6adcd06/AppIcon-0-0-1x_U007emarketing-0-0-0-0-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-181-0-0.png/1024x768bb.png',
+    url: 'https://apps.apple.com/us/app/happy-new-year-2023-animated/id1544641492',
+    genre: 'Stickers'
+  },
+  {
+    name: 'Chinese New Year 2024',
+    artwork: 'https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/6a/16/ed/6a16ed73-2549-a263-5029-23f85d973902/AppIcon-0-0-1x_U007emarketing-0-0-85-181.png/1024x768bb.png',
+    url: 'https://apps.apple.com/us/app/chinese-new-year-2024-%E6%96%B0%E5%B9%B4%E5%BF%AB%E4%B9%90/id1605232842',
+    genre: 'Stickers'
+  },
+  {
+    name: 'Year of the Dragon Animated',
+    artwork: 'https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/c2/e6/2d/c2e62d41-53ac-5af2-241f-605842a5e060/AppIcon-0-0-1x_U007emarketing-0-0-85-181.png/1024x768bb.png',
+    url: 'https://apps.apple.com/us/app/year-of-the-dragon-animated/id6476821446',
+    genre: 'Stickers'
+  },
+  {
+    name: 'Year of the Dragon 2024',
+    artwork: 'https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/cb/a8/ca/cba8ca73-ef08-3a79-9ab6-68f095c05244/AppIcon-0-0-1x_U007emarketing-0-0-85-181.png/1024x768bb.png',
+    url: 'https://apps.apple.com/us/app/year-of-the-dragon-2024/id6476821576',
+    genre: 'Stickers'
+  },
+  {
+    name: 'Year of the Rabbit Animated',
+    artwork: 'https://is1-ssl.mzstatic.com/image/thumb/Purple123/v4/98/cc/0a/98cc0a5a-fd24-7a47-5577-e4b27aadcfd9/AppIcon-0-0-1x_U007emarketing-0-0-0-0-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-181-0-0.png/1024x768bb.png',
+    url: 'https://apps.apple.com/us/app/year-of-the-rabbit-animated/id1664335344',
+    genre: 'Stickers'
+  },
+  {
+    name: 'Year of the Rabbit 新年快乐',
+    artwork: 'https://is1-ssl.mzstatic.com/image/thumb/Purple113/v4/d3/70/ba/d370ba67-257b-3f94-feb0-85ce858757db/AppIcon-0-0-1x_U007emarketing-0-0-0-0-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-181-0-0.png/1024x768bb.png',
+    url: 'https://apps.apple.com/us/app/year-of-the-rabbit-新年快乐/id1666026367',
+    genre: 'Stickers'
+  },
+  {
+    name: 'Year of the Tiger',
+    artwork: 'https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/12/d5/de/12d5de80-7822-2f2f-65c1-cdbcf0211a10/AppIcon-0-0-1x_U007emarketing-0-0-0-0-0-5-sRGB-0-0-0-GLES2_U002c0-512MB-85-181-0-0.png/1024x768bb.png',
+    url: 'https://apps.apple.com/us/app/year-of-the-tiger-新年快乐/id1605231067',
+    genre: 'Stickers'
+  },
+  {
+    name: 'Best Father Stickers',
+    artwork: 'https://is3-ssl.mzstatic.com/image/thumb/Purple113/v4/d5/02/5e/d5025ef3-5ed3-eb9e-a521-397732b845ca/AppIcon-0-0-1x_U007emarketing-0-0-0-0-0-5-sRGB-0-0-0-GLES2_U002c0-512MB-85-181-0-0.png/460x344sr.png',
+    url: 'https://apps.apple.com/us/app/best-father-stickers/id1516141429?ls=1',
+    genre: 'Stickers'
+  },
+  {
+    name: 'Happy Birthday Greetings Pack',
+    artwork: 'https://is4-ssl.mzstatic.com/image/thumb/Purple118/v4/03/61/c4/0361c4af-b556-41d2-dc07-bd9867937507/iMessage_App_Icon-1x_U007emarketing-0-0-GLES2_U002c0-512MB-sRGB-0-0-0-85-181-0-0-0-0.png/460x344sr.png',
+    url: 'https://apps.apple.com/us/app/happy-birthday-greetings-pack/id1410594384?ls=1',
+    genre: 'Stickers'
+  },
+  {
+    name: 'Everyday Greetings and Texts',
+    artwork: 'https://is1-ssl.mzstatic.com/image/thumb/Purple128/v4/70/f5/49/70f549f9-449b-199b-b709-bc2256da494c/iMessage_App_Icon-1x_U007emarketing-0-0-GLES2_U002c0-512MB-sRGB-0-0-0-85-181-0-0-0-0.png/460x344sr.png',
+    url: 'https://apps.apple.com/us/app/everyday-greetings-and-texts/id1165324424?uo=4',
+    genre: 'Stickers'
+  },
+  {
+    name: 'Pinoy Talk',
+    artwork: 'https://is3-ssl.mzstatic.com/image/thumb/Purple128/v4/09/7a/3c/097a3cfc-d7ad-5d3a-a465-c847bac2e649/iMessage_App_Icon-1x_U007emarketing-0-0-GLES2_U002c0-512MB-sRGB-0-0-0-85-181-0-0-0-0.png/460x344sr.png',
+    url: 'https://apps.apple.com/us/app/pinoy-talk/id1387746990?uo=4',
+    genre: 'Stickers'
+  },
+  {
+    name: "It's a Boy! iMessage Stickers",
+    artwork: 'https://is1-ssl.mzstatic.com/image/thumb/Purple125/v4/92/aa/12/92aa124d-a91f-2602-88b7-f0b33d32b4e2/iMessage_App_Icon-1x_U007emarketing-0-0-GLES2_U002c0-512MB-sRGB-0-0-0-85-181-0-0-0-0.png/460x344sr.png',
+    url: 'https://apps.apple.com/us/app/its-a-boy-imessage-stickers/id1387706378?ls=1',
+    genre: 'Stickers'
+  },
+  {
+    name: 'Chinese New Year Blessings',
+    artwork: 'https://is1-ssl.mzstatic.com/image/thumb/Purple123/v4/da/c8/f8/dac8f855-08f9-1ffb-f1bf-c55cb397513d/iMessage_App_Icon-0-0-1x_U007emarketing-0-0-0-0-0-3-sRGB-0-0-0-GLES2_U002c0-512MB-85-181-0-0.png/460x344sr.png',
+    url: 'https://apps.apple.com/us/app/chinese-new-year-blessings/id1494554899?ls=1',
+    genre: 'Stickers'
+  }
+];
